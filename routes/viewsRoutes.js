e695c05a05a0bc2a6789bbf61cdea2099b8ce6db
Milan Rawat <milanrawat086@gmail.com>
2021-02-05 22:25:53 +0530

    Replaced middleware bug fixed

--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -5,12 +5,8 @@ const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get(
-  '/',
-  bookingController.createBookingCheckout,
-  authController.isLoggedIn,
-  viewsController.getOverview
-);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
+
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
